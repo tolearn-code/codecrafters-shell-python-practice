2d13c5225bedbec91778300fb5b0a85c2dcbd95e
tolearn-code <agrawal.gaurav.cl@gmail.com>
2026-04-05 08:15:29 +0000

    added exit

--- app/main.py
+++ app/main.py
@@ -5,7 +5,11 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command_line = input()
+        if command_line.strip().startswith("exit"):
+            sys.exit(0)
+        
         print(f"{command_line}: command not found")
+
     
 
 if __name__ == "__main__":
