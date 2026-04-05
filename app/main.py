4a3f93db479b3ae3d79841c24459758994cea415
tolearn-code <agrawal.gaurav.cl@gmail.com>
2026-04-05 08:10:33 +0000

    completed Handle invalid commands

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    command_line = input()
+    print(f"{command_line}: command not found")
     
 
 if __name__ == "__main__":
