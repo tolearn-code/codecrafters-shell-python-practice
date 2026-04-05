1340ade111c1d3e370a6b3c9f5fc2578adb87c56
tolearn-code <agrawal.gaurav.cl@gmail.com>
2026-04-05 08:26:59 +0000

    added echo

--- app/main.py
+++ app/main.py
@@ -5,9 +5,13 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command_line = input()
-        if command_line.strip().startswith("exit"):
+        command_line = command_line.strip()
+        if command_line.startswith("exit"):
             sys.exit(0)
-        
+        elif command_line.startswith("echo"):
+            to_print = command_line.split("echo ")[1]
+            print(to_print)
+            continue
         print(f"{command_line}: command not found")
 
     
